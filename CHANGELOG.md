a899438f805f14418700f9b188e75e3a1e0f4601
Gelin Luo <greenlaw110@gmail.com>
2017-12-20 14:24:23 +1100

    apply new build system

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Act-Velocity Change Log
+
+1.2.0 - 19/Dec/2017
+* catch up to act-1.6.0
+
+1.1.0
+* catch up to act-1.5.0
+
+1.0.0
+- first formal release
+
+0.7.0
+  - update to ActFramework 0.7.0
+
+0.6.0
+  - update to ActFramework 0.6.0
+
+0.3.0
+  - update to ActFramework 0.3.0
+
+0.2.0
+  - baseline version
--- src/main/java/act/view/velocity/VelocityView.java
+++ src/main/java/act/view/velocity/VelocityView.java
@@ -1,5 +1,25 @@
 package act.view.velocity;
 
+/*-
+ * #%L
+ * ACT Velocity
+ * %%
+ * Copyright (C) 2017 ActFramework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static org.apache.velocity.runtime.RuntimeConstants.RESOURCE_LOADER;
 
 import act.app.App;
@@ -20,13 +40,18 @@ import org.osgl.$;
 import org.osgl.util.E;
 import org.osgl.util.IO;
 import org.osgl.util.S;
+import osgl.version.Version;
+import osgl.version.Versioned;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Properties;
 
+@Versioned
 public class VelocityView extends View {
+
+    public static final Version VERSION = Version.get();
 
     public static final String ID = "velocity";
 
